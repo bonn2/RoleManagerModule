7d2260be809addd5bbef8a5cf53bf0121ffc211f
Brett Bonifas <42011672+bonn2@users.noreply.github.com>
2022-09-14 16:10:40 -0400

    Initial commit

---
+++ src/main/java/net/bonn2/rolemanager/Listener.java
@@ -0,0 +1,171 @@
+package net.bonn2.rolemanager;
+
+import net.bonn2.Bot;
+import net.bonn2.modules.Module;
+import net.bonn2.modules.settings.Settings;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.SelectMenuInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.selections.SelectMenu;
+import net.dv8tion.jda.api.interactions.components.selections.SelectOption;
+import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nonnull;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Listener extends ListenerAdapter {
+
+    private static final int max_selections = 25;
+    private final Module module;
+
+    public Listener(Module module) {
+        this.module = module;
+    }
+
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        if (!event.getName().equals("rolemenu")) return;
+
+        switch (Objects.requireNonNull(event.getSubcommandName())) {
+            case "new" -> {
+                // Validate min/max
+                if (event.getOption("min_selection").getAsLong() < 0) {
+                    event.reply("Your minimum cannot be below 0!").setEphemeral(true).queue();
+                    return;
+                }
+                if (event.getOption("max_selection").getAsLong() < event.getOption("min_selection").getAsLong()) {
+                    event.reply("You maximum cannot be less than your minimum!").setEphemeral(true).queue();
+                    return;
+                }
+
+                // Create embed
+                EmbedBuilder embedBuilder = new EmbedBuilder();
+                embedBuilder.setTitle(event.getOption("title").getAsString());
+                embedBuilder.setDescription(event.getOption("description").getAsString().replaceAll("\\\\n", "\n"));
+                embedBuilder.setColor(Color.CYAN);
+
+                // Create selection menu
+                SelectMenu.Builder selectionBuilder = SelectMenu.create("rolemenu")
+                        .setMinValues((int) event.getOption("min_selection").getAsLong())
+                        .setMaxValues(
+                                event.getOption("max_selection").getAsLong() > 25 ?
+                                        25 : (int) event.getOption("max_selection").getAsLong()
+                        )
+                        .setPlaceholder(event.getOption("placeholder").getAsString());
+
+                // Add roles
+                for (int i = 1; i <= max_selections; i++) {
+                    OptionMapping option = event.getOption("role_" + i);
+                    if (option == null) continue;
+                    Role role = option.getAsRole();
+                    selectionBuilder.addOption(role.getName(), "role_select_" + role.getId());
+                }
+
+                // Build final message
+                MessageCreateBuilder messageBuilder = new MessageCreateBuilder();
+                messageBuilder.setEmbeds(embedBuilder.build());
+                messageBuilder.setActionRow(selectionBuilder.build());
+
+                // Send message
+                event.getChannel().sendMessage(messageBuilder.build()).queue();
+
+                // Confirm
+                event.reply("Posted!").setEphemeral(true).queue();
+            }
+            case "add" -> {
+                // Defer reply as we will need to query discord servers, which may take a while.
+                event.deferReply(true).complete();
+
+                // Get guild channel and message id from provided link
+                Pattern pattern = Pattern.compile("[0-9]+/[0-9]+/[0-9]+");
+                Matcher matcher = pattern.matcher(event.getOption("message_link").getAsString());
+                if (!matcher.find()) {
+                    event.getHook().editOriginal("That is not a valid message link!").queue();
+                    return;
+                }
+                String match = matcher.group();
+                String[] messageLocation = match.split("/");
+                String guildID   = messageLocation[0];
+                String channelID = messageLocation[1];
+                String messageID = messageLocation[2];
+
+                // Use ids to retrieve message
+                Guild guild = Bot.jda.getGuildById(guildID);
+                if (guild == null) {
+                    event.getHook().editOriginal("Failed to get guild!").queue();
+                    return;
+                }
+                MessageChannel messageChannel = guild.getChannelById(MessageChannel.class, channelID);
+                if (messageChannel == null) {
+                    event.getHook().editOriginal("Failed to get message channel!").queue();
+                    return;
+                }
+                Message message = messageChannel.retrieveMessageById(messageID).complete();
+                if (message == null) {
+                    event.getHook().editOriginal("Failed to retrieve message!").queue();
+                    return;
+                }
+                List<ActionRow> actionRows = message.getActionRows();
+                if (actionRows.size() == 1) {
+
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onSelectMenuInteraction(@Nonnull SelectMenuInteractionEvent event) {
+        if (!event.getComponentId().equals("rolemenu")) return;
+        event.deferReply(true).queue();
+        Member member = event.getMember();
+        assert member != null;
+        List<Role> addedRoles = new ArrayList<>();
+        // Add selected roles
+        for (String value : event.getValues()) {
+            // Only add allowed roles
+            String id = value.split("_")[2];
+            // Get and add role
+            Role role = event.getGuild().getRoleById(id);
+            if (role == null) continue;
+            addedRoles.add(role);
+            event.getGuild().addRoleToMember(member, role).queue();
+        }
+        // Remove deselected roles
+        for (SelectOption option : event.getComponent().getOptions()) {
+            // Don't remove selected roles
+            if (event.getValues().contains(option.getValue())) continue;
+            // Only remove allowed roles
+            String id = option.getValue().split("_")[2];
+            // Get and remove role
+            Role role = event.getGuild().getRoleById(id);
+            if (role == null) continue;
+            event.getGuild().removeRoleFromMember(member, role).queue();
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Roles successfully updated!\n" +
+                "Your new roles are: ");
+        for (Role role : addedRoles)
+            stringBuilder.append("<@&%s> ".formatted(role.getId()));
+
+        event.getHook().editOriginal(stringBuilder.toString()).queue();
+    }
+
+    @Override
+    public void onGuildMemberJoin(@Nonnull GuildMemberJoinEvent event) {
+        List<Role> roles = Settings.get(module, event.getGuild().getId(), "default_roles").getAsRoleList(event.getGuild());
+        for (Role role : roles) {
+            event.getGuild().addRoleToMember(event.getMember(), role).queue();
+        }
+    }
+}
---
+++ src/main/java/net/bonn2/rolemanager/Main.java
@@ -0,0 +1,194 @@
+package net.bonn2.rolemanager;
+
+import net.bonn2.Bot;
+import net.bonn2.modules.Module;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+
+public class Main extends Module {
+
+    @Override
+    public void registerLoggingChannels() {}
+
+    @Override
+    public void registerSettings() {
+
+    }
+
+    @Override
+    public void load() {
+        Bot.jda.addEventListener(new Listener(this));
+    }
+
+    @Override
+    public CommandData[] getCommands() {
+        return new CommandData[] {
+                Commands.slash(
+                        "rolemenu",
+                        "Root command for RoleManager."
+                ).addSubcommands(
+                        new SubcommandData(
+                                "add",
+                                "Add a role to a menu."
+                        ).addOption(
+                                OptionType.STRING,
+                                "message_link",
+                                "The link to the selector to edit.",
+                                true
+                        ).addOption(
+                                OptionType.ROLE,
+                                "role",
+                                "The role to add.",
+                                true
+                        ),
+                        new SubcommandData(
+                                "remove",
+                                "Remove a role from a menu."
+                        ).addOption(
+                                OptionType.STRING,
+                                "message_link",
+                                "The link to the selector to edit.",
+                                true
+                        ).addOption(
+                                OptionType.ROLE,
+                                "role",
+                                "The role to remove.",
+                                true
+                        ),
+                        new SubcommandData(
+                                "new",
+                                "Create a role selection menu."
+                        ).addOption(
+                                OptionType.STRING,
+                                "title",
+                                "The title of the embed.",
+                                true
+                        ).addOption(
+                                OptionType.STRING,
+                                "description",
+                                "The description of the embed. You can use \\n for newlines",
+                                true
+                        ).addOption(
+                                OptionType.INTEGER,
+                                "min_selection",
+                                "The minimum number of roles a user can select.",
+                                true
+                        ).addOption(
+                                OptionType.INTEGER,
+                                "max_selection",
+                                "The maximum number of roles a user can select.",
+                                true
+                        ).addOption(
+                                OptionType.STRING,
+                                "placeholder",
+                                "What the selection will show when nothing is selected.",
+                                true
+                        ).addOption(
+                                OptionType.ROLE,
+                                "role_1",
+                                "The first role.",
+                                true
+                        ).addOption(
+                                OptionType.ROLE,
+                                "role_2",
+                                "The second role.",
+                                false
+                        ).addOption(
+                                OptionType.ROLE,
+                                "role_3",
+                                "The third role.",
+                                false
+                        ).addOption(
+                                OptionType.ROLE,
+                                "role_4",
+                                "The fourth role.",
+                                false
+                        ).addOption(
+                                OptionType.ROLE,
+                                "role_5",
+                                "The fifth role.",
+                                false
+                        ).addOption(
+                                OptionType.ROLE,
+                                "role_6",
+                                "The sixth role.",
+                                false
+                        ).addOption(
+                                OptionType.ROLE,
+                                "role_7",
+                                "The seventh role.",
+                                false
+                        ).addOption(
+                                OptionType.ROLE,
+                                "role_8",
+                                "The eighth role.",
+                                false
+                        ).addOption(
+                                OptionType.ROLE,
+                                "role_9",
+                                "The ninth role.",
+                                false
+                        ).addOption(
+                                OptionType.ROLE,
+                                "role_10",
+                                "The tenth role.",
+                                false
+                        ).addOption(
+                                OptionType.ROLE,
+                                "role_11",
+                                "The eleventh role.",
+                                false
+                        ).addOption(
+                                OptionType.ROLE,
+                                "role_12",
+                                "The twelfth role.",
+                                false
+                        ).addOption(
+                                OptionType.ROLE,
+                                "role_13",
+                                "The thirteenth role.",
+                                false
+                        ).addOption(
+                                OptionType.ROLE,
+                                "role_14",
+                                "The fourteenth role.",
+                                false
+                        ).addOption(
+                                OptionType.ROLE,
+                                "role_15",
+                                "The fifteenth role",
+                                false
+                        ).addOption(
+                                OptionType.ROLE,
+                                "role_16",
+                                "The sixteenth role",
+                                false
+                        ).addOption(
+                                OptionType.ROLE,
+                                "role_17",
+                                "The seventeenth role.",
+                                false
+                        ).addOption(
+                                OptionType.ROLE,
+                                "role_18",
+                                "The eighteenth role.",
+                                false
+                        ).addOption(
+                                OptionType.ROLE,
+                                "role_19",
+                                "The nineteenth role.",
+                                false
+                        ).addOption(
+                                OptionType.ROLE,
+                                "role_20",
+                                "The twentieth role.",
+                                false
+                        )
+                ).setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_ROLES))
+        };
+    }
+}
